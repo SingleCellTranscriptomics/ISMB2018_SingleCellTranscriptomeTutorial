a9694776f1047a79e651cd052a041d76a67da164
brianjohnhaas <brianjohnhaas@gmail.com>
2018-06-21 16:38:20 +0000

    update

--- docker/installR.sh
+++ docker/installR.sh
@@ -4,10 +4,13 @@
 export DISTRIBUTION_CODENAME=`grep DISTRIB_CODENAME /etc/lsb-release | cut -c 18-`
 
 # Appends the CRAN repository to your sources.list file
-sh -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu $DISTRIBUTION_CODENAME/" >> /etc/apt/sources.list'
+#sh -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu $DISTRIBUTION_CODENAME/" >> /etc/apt/sources.list'
+sh -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/" >> /etc/apt/sources.list'
 
 # Add GPG key of Michael Rutter the Ubuntu archives CRAN maintainer
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install -y r-base r-base-dev
